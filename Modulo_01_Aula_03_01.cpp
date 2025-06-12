0508914b47933ac025a2061a1360e97a74fb3c04
Antonio_Souto_BV_Linux <antonio.rodriguez@icomp.ufam.edu.br.com>
2025-06-11 20:38:49 -0400

    Aula 02

--- Modulo_01_Aula_03_01.cpp
+++ Modulo_01_Aula_03_01.cpp
@@ -25,8 +25,11 @@ if (image_Input.is_open()) {
 
 // Header !!
 image_Input << "P3" << endl;
-image_Input << "3 2" << endl;
+image_Input << "70 50" << endl;
 image_Input << "255" << endl;
+
+image_Input >>
+
 
 // Image Body  -  Size 3 x 2 -- RGB triplets
 
@@ -37,10 +40,31 @@ image_Input << "255" << endl;
 // image_Input << "255 255 255 " ;  //white
 // image_Input << "0 0 0" << endl;		 //black
 
-image_Input << "255 0 0 0 255 0 0 0 255" << endl;
-image_Input << "255 255 0 255 255 255 0 0 0" << endl;		
+//  image_Input << "255 0 0 0 255 0 0 0 255" << endl;
+//  image_Input << "255 255 0 255 255 255 0 0 0" << endl;		
+
+// 50% RED e 50% Blue
+
+for (int ImgH = 0; ImgH < 70; ImgH++) {
+    if (ImgH % 2 == 0) {
+        for(int ImhV = 0; ImhV < 50; ImhV++) {
+            image_Input << "255 0 0 ";		// Red
+        }
+    } else {
+        for(int ImhV = 0; ImhV < 50; ImhV++) {
+            image_Input << "0 0 255 ";		// Blue
+        }
+    }
+    // for(int ImhV = 0; ImhV < 50; ImhV++) {
+    //     image_Input << "255 0 0 ";		// Red
+    // }
+
+
+    image_Input << endl;
+}
 }
 
+// Close the file
 
 image_Input.close();
     
